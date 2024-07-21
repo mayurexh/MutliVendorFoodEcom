2f4304bf5381f3af3617d204b96255ede6870201
mayurexh <mayuresh.pisat111@gmail.com>
2024-07-21 23:52:59 +0530

    User login and restrictions

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,31 @@ from django.http import HttpResponse
 from .forms import UserForm
 from vendor.forms import VendorForm
 from .models import *
-from django.contrib import messages
+from django.contrib import messages, auth
 from vendor.models import Vendor
-
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required,user_passes_test
+from .utils import detect_user
+from django.core.exceptions import PermissionDenied
 # Create your views here.
+#custom decorator to restrict vendor from accessing customer
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+#custom decorator to restrict customer from accessing vendor
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
 
 def registerUser(request):
+    if request.user.is_authenticated:
+        return redirect("dashboard")
     msg = ""
     if request.method == "POST":
         form = UserForm(request.POST)
@@ -72,3 +91,46 @@ def registerVendor(request):
     v_form = VendorForm()
     return render(request, "accounts/registerVendor.html", {"form":form,
                                                             "v_form":v_form})
+    
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "Wait you are already logged in")
+        return redirect("myAccount")
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = auth.authenticate( email = email, password = password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are logged in")
+            return redirect("myAccount")
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect("login")
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url="login")
+def logout(request):
+    auth.logout(request)
+    messages.info(request, "you are logged out")
+    return redirect("login")
+
+
+@login_required(login_url="login")
+@user_passes_test(check_role_customer)
+def customerDashboard(request):
+    return render(request, "accounts/customerDashboard.html")
+
+
+
+@login_required(login_url="login")
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request, "accounts/vendorDashboard.html")
+
+@login_required(login_url="login")
+def myAccount(request):
+    user = request.user
+    redirectUrl = detect_user(user)
+    return redirect(redirectUrl)
